a04aa1ba50493573636babec913569362b46a80f
feiyang1128 <75651216+feiyang1128@users.noreply.github.com>
2025-08-30 18:23:56 +0800

    Update openwrt.sh

--- openwrt.sh
+++ openwrt.sh
@@ -37,7 +37,11 @@ trap 'cleanup_tmp_dir; echo -e "${RED}脚本已退出${NC}"; exit 1' INT TERM EX
 get_latest_github_version() {
     repo="$1"
     echo -e "${YELLOW}正在获取 GitHub 仓库 $repo 的最新版本...${NC}"
-    latest_version=$(curl -s "https://ghproxy.feiyang.gq/https://api.github.com/repos/$repo/releases" | jq -r '.[0].tag_name')
+       # 请求 GitHub API 并抓取标准版本号
+    latest_version=$(curl -s "https://ghproxy.feiyang.gq/https://api.github.com/repos/$repo/releases" \
+        | grep '"tag_name":' \
+        | grep -Eo 'v[0-9]+\.[0-9]+\.[0-9]+' \
+        | head -n 1)
     echo "$latest_version"
 }
 
