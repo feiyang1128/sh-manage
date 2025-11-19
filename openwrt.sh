f16177a6b574e7585f009361c5c171da86c2131c
feiyang1128 <75651216+feiyang1128@users.noreply.github.com>
2025-11-19 22:43:28 +0800

    Update openwrt.sh

--- openwrt.sh
+++ openwrt.sh
@@ -4,7 +4,7 @@
 # ====== 脚本配置 ========
 REMOTE_SCRIPT_URL="https://sh.feiyang.gq/openwrt.sh"
 LOCAL_SCRIPT_PATH="/root/opt.sh"
-GITHUB_PROXY="https://gh-proxy.org/"
+GITHUB_PROXY="https://ghproxy.feiyang.gq/"
 TMP_DIR="/tmp/install_tmp"
 # ====== 软件配置 ======
 OPENCLASH_REPO="vernesong/OpenClash"
